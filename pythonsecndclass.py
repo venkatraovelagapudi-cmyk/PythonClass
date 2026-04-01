c3e9afea37b059bc50fba9bb3437ad1c2688841d
Venkatrao_Velagapudi <venkatraovelagapudi@gmail.com>
2026-04-01 14:09:38 +0530

    Add files via upload

---
+++ pythonsecndclass.py
@@ -0,0 +1,10 @@
+a=10;b=20
+print("Arthematic Operators")
+print(f"Addition of {a,b}={a+b}\n",
+      f"Subtraction of {a,b}={a-b}\n",
+      f"Multiplication of {a,b}={a*b}\n",
+      f"Division of {a,b}={a/b}\n",
+      f"Modulus of {a,b}={a%b}\n",
+      f"Floor Division of {a,b}={a//b}\n",
+      f"Exponent of {a,b}={a**b}\n")
+      
